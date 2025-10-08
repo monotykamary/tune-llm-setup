3501c4758e4650341000584ebc7ee462bdb70cb7
Tom X Nguyen <tom81094@gmail.com>
2025-10-08 23:38:27 +0700

    chore: add new gpt and grok models

---
+++ gpt-5-mini.llm.mjs
@@ -0,0 +1,16 @@
+export default async (payload, context) => {
+  const key = await context.read("OPENAI_API_KEY");
+
+  return ({
+    url: "https://api.openai.com/v1/chat/completions",
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      authorization: `Bearer ${key}`
+    },
+    body: JSON.stringify({
+      ...payload,
+      model: "gpt-5-mini",
+    })
+  })
+}
---
+++ grok-4-fast.llm.mjs
@@ -0,0 +1,16 @@
+export default async (payload, context) => {
+  const key = await context.read("GROK_API_KEY");
+
+  return ({
+    url: "https://api.x.ai/v1/chat/completions",
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      authorization: `Bearer ${key}`
+    },
+    body: JSON.stringify({
+      ...payload,
+      model: "grok-4-fast",
+    })
+  })
+}
